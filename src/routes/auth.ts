4eef5f26f37fe05e2c7aca6e7510437431810356
raynaud0 <bolivez@hotmail.com>
2020-12-27 16:45:07 +0100

    register login me are done by using

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,5 +1,8 @@
 import { Request, Response, Router } from "express"
-import { validate } from "class-validator"
+import { isEmpty, validate } from "class-validator"
+import bcrypt from "bcrypt"
+import jwt from "jsonwebtoken"
+import cookie from "cookie"
 
 import { User } from "../entities/User"
 
@@ -35,7 +38,62 @@ const register = async (req: Request, res: Response) => {
   }
 }
 
+const login = async (req: Request, res: Response) => {
+  const { username, password } = req.body
+  try {
+    let errors: any = {}
+    if (isEmpty(username)) errors.username = "Username must not be empty"
+    if (isEmpty(password)) errors.password = "Password must not be empty"
+    if (Object.keys(errors).length > 0) {
+      return res.status(400).json(errors)
+    }
+
+    const user = await User.findOne({ username })
+    if (!user) return res.status(404).json({ error: "User not found " })
+
+    const passwordMatches = await bcrypt.compare(password, user.password)
+
+    if (!passwordMatches) {
+      return res.status(401).json({ password: "Password is incorrect" })
+    }
+
+    const token = jwt.sign({ username }, process.env.JWT_SECRET)
+
+    res.set(
+      "Set-Cookie",
+      cookie.serialize("token", token, {
+        httpOnly: true,
+        secure: process.env.NODE_ENV === "production",
+        sameSite: "strict",
+        maxAge: 3600,
+        path: "/",
+      })
+    )
+
+    return res.json(user)
+  } catch (err) {}
+}
+
+const me = async (req: Request, res: Response) => {
+  try {
+    const token = req.cookies.token
+    if (!token) throw new Error("Unauthenticated")
+
+    const { username }: any = jwt.verify(token, process.env.JWT_SECRET)
+
+    const user = await User.findOne({ username })
+    if (!user) throw new Error("Unauthenticated")
+
+    return res.json(user)
+  } catch (err) {
+    console.log(err)
+    return res.status(401).json({ error: err.message })
+  }
+}
+
 const router = Router()
 router.post("/register", register)
+router.get("/login", login)
+router.get("/me", me)
 
 export default router
--- src/server.ts
+++ src/server.ts
@@ -2,21 +2,25 @@ import "reflect-metadata"
 import { createConnection } from "typeorm"
 import express from "express"
 import morgan from "morgan"
+import dotenv from "dotenv"
+import cookieParser from "cookie-parser"
 
 import authRoutes from "./routes/auth"
-
 import trim from "./middleware/trim"
+
+dotenv.config()
 
 const app = express()
 
 app.use(express.json())
 app.use(morgan("dev"))
 app.use(trim)
+app.use(cookieParser())
 
 app.get("/", (_, res) => res.send("Hello World"))
 app.use("/api/auth", authRoutes)
 
-app.listen(5000, async () => {
+app.listen(process.env.PORT, async () => {
   console.log("Server running at http://localhost:5000")
 
   try {
